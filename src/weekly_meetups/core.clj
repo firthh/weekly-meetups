8f8907acac6b7887222fdfab3adc3b11f7b6f8bd
Hugo Firth <hugo.firth@gmail.com>
2014-02-18 23:39:53 +1000

    reading event about multiple meetups

--- src/weekly_meetups/core.clj
+++ src/weekly_meetups/core.clj
@@ -1,14 +1,35 @@
 (ns weekly-meetups.core
-  (require [clojure.data.json :as json])
+  (:require [clojure.data.json :as json])
+  (:use [clj-time.core]
+        [clj-time.coerce])
   (:gen-class))
 
+
+(def meetups
+  ["clj-bne" "qldjvm"])
 
 (def meetup-url 
   "http://api.meetup.com/2/events?sign=true&key=%s&group_urlname=%s")
 
-(defn -main [api-key]
-  (-> (format meetup-url api-key "clj-bne") 
+(defn- in-a-week? [event]
+  (before? (from-long (get event "time")) (plus (now) (weeks 3)))
+  )
+
+(defn- get-meetup-events [api-key meetup]
+  (-> (format meetup-url api-key meetup) 
       slurp
       json/read-str
       (get "results")
       ))
+
+(defn- get-all-meetups [api-key]
+  (->>
+   (map #(get-meetup-events api-key %) meetups)
+   flatten
+   ))
+
+(defn -main [api-key]
+  (filter 
+   in-a-week?
+   (get-all-meetups api-key))
+    )
