cbfd97514b32a6221d040c31df108b4780919749
Hugo Firth <hugo.firth@gmail.com>
2014-02-21 16:59:45 +1000

    added new endpoints to webserver

--- src/weekly_meetups/web.clj
+++ src/weekly_meetups/web.clj
@@ -5,7 +5,11 @@
             [compojure.handler :as handler]))
 
 (defroutes app-routes
-  (GET "/:api-key" [api-key] (core/events-to-html (core/get-events api-key :brisbane))))
+  (GET "/api-key/:api-key" [api-key] 
+       (core/events-to-html (core/get-events api-key :brisbane)))
+  (GET "/api-key/:api-key/city/:city" [api-key city]
+       (core/events-to-html (core/get-events api-key (keyword city))))
+  (GET "/" [] "Hello"))
 
 (def app
   (handler/site app-routes))
